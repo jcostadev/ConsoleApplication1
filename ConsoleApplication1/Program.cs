6b12e45e5772c701ce67b80c09e2cd72534f419f
Juliana Costa <jcosta@mpagg.local>
2016-09-15 15:34:50 -0300

    First commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            //File.AppendAllText(@"C:\file.txt", "text content");
+
+            Console.ReadLine();
+        }
+    }
+}
